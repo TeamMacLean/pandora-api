092d1c51f69849b082f8d21e054236c4be723ada
Martin Page <wookoouk@gmail.com>
2020-06-23 12:11:57 +0100

    import species util

---
+++ utils/import_species.js
@@ -0,0 +1,22 @@
+const xlsx = require('xlsx')
+const Species = require('../models/Species');
+const workbook = xlsx.readFile('Specieslist.xlsx');
+
+Promise.all(
+	workbook.Strings.map(s=>{
+		Species.find({name:s.t})
+		.then(found=>{
+			if(!found.length){
+				return new Species({name:s.t}).save()
+			} else {
+				return Promise.resolve()
+			}
+		})
+	})
+)
+.then(()=>{
+	console.log('done');
+})
+.catch(err=>{
+	console.error(err);
+})
